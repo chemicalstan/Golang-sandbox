995ba4d8558fa5459d58d2f1f837174375662f11
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2022-03-05 16:19:51 +0100

    Implement save to file receiver func

--- CARDS/deck.go
+++ CARDS/deck.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
 // Create a new type of deck
 // Which is a slice of string
 type deck []string
@@ -21,4 +27,18 @@ func (d deck) deal(handSize int) (deck, deck) {
 	start := d[:handSize]
 	rest := d[handSize:]
 	return start, rest
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
